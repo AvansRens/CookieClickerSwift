f1fa2146d60e09668fb6d90dccc734ea196a3680
Rens <rm.aarts@student.avans.nl>
2026-02-05 08:56:23 +0100

    Initial Commit

---
+++ CookieClicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookieClicker
+//
+//  Created by Rens Aarts on 05/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
